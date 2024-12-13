2fe5c8224290e795ca92f914de04184bff78fc04
Erhan BÜTE <erhan@hey.com>
2024-12-14 01:43:34 +0300

    Update client.py

--- pyparam/client.py
+++ pyparam/client.py
@@ -121,3 +121,61 @@ class Client:
 
         }
         return self._request('TP_Islem_Odeme_BKM', is_hashed=True, **data)
+
+
+
+    def add_card(self, merchant_guid, card_holder, card_number, card_expiration_month, card_expiration_year, card_alias, transaction_id):
+        data = {
+            'GUID': merchant_guid,
+            'KK_Sahibi': card_holder,
+            'KK_No': card_number,
+            'KK_SK_Ay': card_expiration_month,
+            'KK_SK_Yil': card_expiration_year,
+            'KK_Kart_Adi': card_alias,
+            'KK_Islem_ID': transaction_id
+        }
+        return self._request('KS_Kart_Ekle', is_hashed=True, **data)
+
+
+
+    def verify_card(self, card_number, card_expiration_month, card_expiration_year, card_cvc, return_url, data1="", data2="", data3="", data4="", data5=""):
+        data = {
+            'KK_No': card_number,
+            'KK_SK_Ay': card_expiration_month,
+            'KK_SK_Yil': card_expiration_year,
+            'KK_CVC': card_cvc,
+            'Return_URL': return_url,
+            'Data1': data1,
+            'Data2': data2,
+            'Data3': data3,
+            'Data4': data4,
+            'Data5': data5
+
+        }
+        return self._request('TP_KK_Verify', is_hashed=True, **data)
+
+
+    def pay_with_card(self, merchant_guid, card_guid, card_cvv, customer_gsm, fail_url, success_url, reference_id, installment, amount, total_amount, ip_address, kk_transaction_id, is_3d=False, ref_url="", data1="", data2="", data3="", data4="", data5="", description="", order_id=""):
+        data = {
+            'GUID': merchant_guid,
+            'KS_GUID': card_guid,
+            'CVV': card_cvv,
+            'KK_Sahibi_GSM': customer_gsm,
+            'Hata_URL': fail_url,
+            'Basarili_URL': success_url,
+            'Siparis_ID': reference_id,
+            'Siparis_Aciklama': description,
+            'Taksit': installment,
+            'Islem_Tutar': amount,
+            'Toplam_Tutar': total_amount,
+            'Islem_Guvenlik_Tip': '3D' if is_3d else 'NS',
+            'Islem_ID': order_id,
+            'IPAdr': ip_address,
+            'Ref_URL': ref_url,
+            'Data1': data1,
+            'Data2': data2,
+            'Data3': data3,
+            'Data4': data4,
+            'KK_Islem_ID': kk_transaction_id
+        }
+        return self._request('KS_Tahsilat', is_hashed=True, **data)
